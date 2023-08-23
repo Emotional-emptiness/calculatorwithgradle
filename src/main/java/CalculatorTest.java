87663e41e2acb02226119436fcc2a2621bcb86d3
Serg <moon.linexx@gmail.com>
2023-08-23 10:52:09 +0300

    calculator with gradle

---
+++ src/main/java/CalculatorTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class CalculatorTest {
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(15, calculator.multiply(5, 3));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calculator.divide(6, 3));
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(6, 0));
+    }
+}
